72c9bd6360c1783fe08c375e273cd51a8f6d8c9f
Kamihara <yoshihara72@gmail.com>
2021-05-16 15:10:30 +0900

    end tutorial

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,34 @@
 <script>
-	let name = 'world';
+	let text = 'Svelte';
+	let count = 0;
+	const handleClickCount = () => count += 1;
+	let isActive = false;
+	const handleClickActive = () => isActive = !isActive
 </script>
 
-<h1>Hello {name}!</h1>
+<p>Hello {text}!</p>
+
+<p>
+	<input type=text bind:value={text}>
+</p>
+
+<p>
+	<input type=number bind:value={count}>
+	<button on:click="{handleClickCount}">+</button>	
+</p>
+
+<p>
+	<!-- <button on:click="{handleClickActive}" class={isActive ? 'active' : ''}>
+		いいね
+	</button> -->
+	<button on:click="{handleClickActive}" class:active={isActive}>
+		いいね
+	</button>
+</p>
+
+<style>
+	.active {
+		background-color: #ff3e00;
+		color: white;
+	}
+</style>
